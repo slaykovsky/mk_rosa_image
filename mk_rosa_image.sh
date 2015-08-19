f7579bc87b3b5681a4746693224d4cb9bcce5910
Alexey Slaykovsky <alexey@slaykovsky.com>
2015-08-19 18:56:55 +0300

    Init commit

---
+++ mk_rosa_image.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+#
+# Create a base ROSA Docker image.
+#
+#
+
+usage() {
+  cat <<EOOPTS
+$(basename $0) <name>
+ACHTUNG:
+  You need to specify name of resulting tarboll.
+EOOPTS
+  exit 1
+}
+
+yum_config=/etc/yum.conf
+
+shift $((OPTIND -1))
+name=$1
+
+if [[ -z $name ]]; then
+  usage
+fi
+
+target=$(mktemp -d --tpmdir $(basename $0).XXXXXXXX)
+
+set -x
+
+mkdir -m 755 "$target"/dev
+mknod -m 600 "$target"/dev/console c 5 1
+mknod -m 600 "$target"/dev/initctl p
+mknod -m 666 "$target"/dev/full c 1 7
+mknod -m 666 "$target"/dev/null c 1 3
+mknod -m 666 "$target"/dev/ptmx c 5 2
+mknod -m 666 "$target"/dev/random c 1 8
+mknod -m 666 "$target"/dev/tty c 5 0
+mknod -m 666 "$target"/dev/tty0 c 4 0
+mknod -m 666 "$target"/dev/urandom c 1 9
+mknod -m 666 "$target"/dev/zero c 1 5
+
+yum -c "$yum_config" --installroot="$target" --releasever=/ --setopt=tsflags=nodocs \
+      --setopt=group_package_types=mandatory -y groupinstall Core
+yum -c "$yum_config" --installroot="$target" -y clean all
+
+cat > "$target"/etc/sysconfig/network <<EOF
+NETWORKING=yes
+HOSTNAME=localhost.localdomain
+EOF
+
+# effectively: febootstrap-minimize --keep-zoneinfo --keep-rpmdb
+# --keep-services "$target".  Stolen from mkimage-rinse.sh
+#  locales
+rm -rf "$target"/usr/{{lib,share}/locale,{lib,lib64}/gconv,bin/localedef,sbin/build-locale-archive}
+#  docs
+rm -rf "$target"/usr/share/{man,doc,info,gnome/help}
+#  cracklib
+rm -rf "$target"/usr/share/cracklib
+#  i18n
+rm -rf "$target"/usr/share/i18n
+#  sln
+rm -rf "$target"/sbin/sln
+#  ldconfig
+rm -rf "$target"/etc/ld.so.cache
+rm -rf "$target"/var/cache/ldconfig/*
+
+cd "$target"
+
+tar --numeric-owner -cf /root/"$name".tar .
+
+rm -rf "$target"
